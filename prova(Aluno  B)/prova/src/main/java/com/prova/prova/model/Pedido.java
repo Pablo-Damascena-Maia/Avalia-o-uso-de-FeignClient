f3c40d1e3e424f3d567acd929f993ebdca46d55b
Pablo <pablomaia533@gmail.com>
2025-10-24 11:38:30 -0300

    Add files via upload

---
+++ prova(Aluno  B)/prova/src/main/java/com/prova/prova/model/Pedido.java
@@ -0,0 +1,28 @@
+package com.prova.prova.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+    private Double valor;
+    private Long usuarioId;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public Double getValor() { return valor; }
+    public void setValor(Double valor) { this.valor = valor; }
+
+    public Long getUsuarioId() { return usuarioId; }
+    public void setUsuarioId(Long usuarioId) { this.usuarioId = usuarioId; }
+}
