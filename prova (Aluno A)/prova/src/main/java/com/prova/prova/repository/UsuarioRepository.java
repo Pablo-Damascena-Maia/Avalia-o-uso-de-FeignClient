4b3559389e0946d894613f381f6fc95dea23106d
Pablo <pablomaia533@gmail.com>
2025-10-31 11:13:35 -0300

    Delete prova (Aluno A)/prova directory

--- prova (Aluno A)/prova/src/main/java/com/prova/prova/repository/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.prova.prova.repository;
-
-import com.prova.prova.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
