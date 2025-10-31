020a89959ad2dade64a58ea9ea9be0315996ee9f
Pablo <pablomaia533@gmail.com>
2025-10-31 11:15:53 -0300

    Delete prova(Aluno  B)/prova directory

--- prova(Aluno  B)/prova/src/main/java/com/prova/prova/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prova.prova.repository;
-
-import com.prova.prova.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-    List<Pedido> findByUsuarioId(Long usuarioId);
-}
